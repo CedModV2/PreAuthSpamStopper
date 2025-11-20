fc96fdcc89f16b22ad384a2a359f945ea48db401
ced777ric <ced777ric@cedmod.nl>
2025-11-20 21:45:17 +0100

    Initial Commit

---
+++ PreAuthSpamStopper/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using HarmonyLib;
+using LabApi.Features.Console;
+using MEC;
+
+namespace PreAuthSpamStopper
+{
+    public class Plugin: LabApi.Loader.Features.Plugins.Plugin
+    {
+        HarmonyLib.Harmony _harmony = new Harmony("preauthspamstopper");
+        public override void Enable()
+        {
+            Timing.CallDelayed(1f, () =>
+            {
+                if (AppDomain.CurrentDomain.GetAssemblies().Any(s => s.FullName.Contains("cedmod", StringComparison.OrdinalIgnoreCase)))
+                {
+                    Logger.Error("CedMod already includes the functionality of this plugin, this plugin will not be enabled.");
+                    return;
+                }
+                
+                _harmony.PatchAll();
+            });
+        }
+
+        public override void Disable()
+        {
+            _harmony.UnpatchAll(_harmony.Id);
+        }
+
+        public override string Name { get; } = "PreauthSpamStopper";
+        public override string Description { get; } = "PreauthSpamStopper";
+        public override string Author { get; } = "Ced777ric";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version RequiredApiVersion { get; } = new Version(1, 0, 0);
+    }
+}
---
+++ PreAuthSpamStopper/PreAuthPatch.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using CentralAuth;
+using Cryptography;
+using HarmonyLib;
+using LiteNetLib;
+using LiteNetLib.Utils;
+using NorthwoodLib.Pools;
+using SlProxy;
+
+using static HarmonyLib.AccessTools;
+
+namespace CedMod.Addons.QuerySystem.Patches
+{
+    [HarmonyPatch(typeof(CustomLiteNetLib4MirrorTransport), nameof(CustomLiteNetLib4MirrorTransport.ProcessConnectionRequest))]
+    public static class AuthKickTranspiler
+    {
+        public static DateTime LastReset = DateTime.MinValue;
+        public static DateTime LastRejected = DateTime.MinValue;
+        public static int Rejected = 0;
+        
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(codeInstructions);
+
+            int offset = 9;
+            int index = newInstructions.FindIndex(x => x.opcode == OpCodes.Ldsfld && x.operand is FieldInfo { Name: "UseChallenge" }) + offset;
+            
+            Label ret = generator.DefineLabel();
+            
+            newInstructions.InsertRange(index, new[]
+            {
+                // if(!AuthKickTranspiler.MethodTest(request))
+                new CodeInstruction(OpCodes.Ldarg_1).MoveLabelsFrom(newInstructions[index]),
+                CodeInstruction.Call(typeof(AuthKickTranspiler), nameof(PreauthCheck), new []{ typeof(ConnectionRequest) }), 
+                
+                // return;
+                new(OpCodes.Brfalse_S, ret),
+            });
+            
+            newInstructions[^1].labels.Add(ret);
+            
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+
+        public static bool PreauthCheck(ConnectionRequest request)
+        {
+            if ((DateTime.UtcNow - LastReset).TotalHours >= 1 && (DateTime.UtcNow - LastRejected).TotalMinutes >= 15)
+            {
+                LastReset = DateTime.UtcNow;
+                Rejected = 0;
+            }
+            
+            var reader = new NetDataReader(request.Data.RawData);
+            reader._position = 34;
+            var preauthdata = PreAuthModel.ReadPreAuth(reader);
+            if (preauthdata == null)
+            {
+                CustomLiteNetLib4MirrorTransport.RequestWriter.Reset();
+                CustomLiteNetLib4MirrorTransport.RequestWriter.Put((byte)RejectionReason.Custom);
+                CustomLiteNetLib4MirrorTransport.RequestWriter.Put($"[PreAuthSpamStopper]\nYour connection has been rejected as the 'PreAuth' data sent from your client appears to be invalid, please restart your game or run 'ar' in your client console, You can usually open the client console by pressing ` or ~");
+
+                CustomLiteNetLib4MirrorTransport.Rejected++;
+                if (CustomLiteNetLib4MirrorTransport.Rejected > CustomLiteNetLib4MirrorTransport.RejectionThreshold)
+                    CustomLiteNetLib4MirrorTransport.SuppressRejections = true;
+
+                if (!CustomLiteNetLib4MirrorTransport.SuppressRejections && CustomLiteNetLib4MirrorTransport.DisplayPreauthLogs && Rejected <= 5)
+                    ServerConsole.AddLog($"Security challenge response of incoming connection from endpoint {request.RemoteEndPoint} has been CustomLiteNetLib4MirrorTransport.Rejected (Failed extra PreAuthSpamStopper verification 1).");
+
+                LastRejected = DateTime.UtcNow;
+                Rejected++;
+                return false;
+            }
+
+            if (PlayerAuthenticationManager.OnlineMode && !ECDSA.VerifyBytes($"{preauthdata.UserID};{preauthdata.Flags};{preauthdata.Region};{preauthdata.Expiration}", preauthdata.Signature, ServerConsole.PublicKey))
+            {
+                CustomLiteNetLib4MirrorTransport.RequestWriter.Reset();
+                CustomLiteNetLib4MirrorTransport.RequestWriter.Put((byte)RejectionReason.Custom);
+                CustomLiteNetLib4MirrorTransport.RequestWriter.Put($"[PreAuthSpamStopper]\nYour connection has been rejected as the 'PreAuth' data sent from your client appears to be invalid, please restart your game or run 'ar' in your client console, You can usually open the client console by pressing ` or ~");
+                request.RejectForce(CustomLiteNetLib4MirrorTransport.RequestWriter);
+
+                CustomLiteNetLib4MirrorTransport.Rejected++;
+                if (CustomLiteNetLib4MirrorTransport.Rejected > CustomLiteNetLib4MirrorTransport.RejectionThreshold)
+                    CustomLiteNetLib4MirrorTransport.SuppressRejections = true;
+
+                if (!CustomLiteNetLib4MirrorTransport.SuppressRejections && CustomLiteNetLib4MirrorTransport.DisplayPreauthLogs && Rejected <= 5)
+                    ServerConsole.AddLog($"Security challenge response of incoming connection from endpoint {request.RemoteEndPoint} has been CustomLiteNetLib4MirrorTransport.Rejected (Failed extra PreAuthSpamStopper verification 2).");
+                
+                LastRejected = DateTime.UtcNow;
+                Rejected++;
+                return false;
+            }
+            return true;
+        }
+    }
+}
